fffb4a0120ecdce5a05686da12d2132c90adf261
supun19 <supun.12@cse.mrt.ac.lk>
2016-12-02 19:39:08 +0530

    QR Scaner Is Work
     QR scanner detect code
     Four Ui Created Login,Scan,SellerDetail,Finish

---
+++ app/src/main/java/com/example/buddhikajay/mobilepay/qrscanActivity.java
@@ -0,0 +1,19 @@
+package com.example.buddhikajay.mobilepay;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class qrscanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscan);
+
+        Button btn=(Button)findViewById(R.id.qrscan_button);
+        
+    }
+}
