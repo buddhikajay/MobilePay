91fb45e3fab8066f8fa1b89b43d8aaa262f69500
supun19 <supun12.@cse.mrt.ac.lk>
2017-07-21 12:28:00 +0530

    2017/7/21

---
+++ app/src/main/java/com/example/buddhikajay/mobilepay/BillSpliteActivity.java
@@ -0,0 +1,13 @@
+package com.example.buddhikajay.mobilepay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BillSplite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill_splite);
+    }
+}
