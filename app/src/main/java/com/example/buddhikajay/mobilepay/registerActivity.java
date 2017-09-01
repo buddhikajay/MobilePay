1bc9879775eea61ab1f6c8c62a4fc4044872f4e1
supun19 <supun12.@cse.mrt.ac.lk>
2017-09-01 12:46:16 +0530

    Change Api Change to lastname firstname

--- app/src/main/java/com/example/buddhikajay/mobilepay/registerActivity.java
+++ app/src/main/java/com/example/buddhikajay/mobilepay/registerActivity.java
@@ -393,8 +393,9 @@ public class registerActivity extends AppCompatActivity {
                 Log.d("nic",Api.getNic(getApplicationContext()));
                 Log.d("RegisterActivity:phone",Api.getPhoneNumber(getApplication()));
                 //Log.d("verification code",Api.getPhoneNumber(getApplication())+""+jsonObject.opt("verificationCode").toString());
-                DecimalFormat df = new DecimalFormat("0.00##");
-                Api.sendSms(Api.getPhoneNumber(getApplication()),String.format("%.2f", 1.2),getApplicationContext());
+
+                String verificatioCode = jsonObject.opt("verificationCode").toString();
+                Api.sendSms(Api.getPhoneNumber(getApplication()),String.format("%.2f", Double.parseDouble(verificatioCode)),getApplicationContext());
                 finish();
                 moveToPinActivity();
                 //showmessgebox();
